8a6ee28caff3e9e367343fa2dccb9c34e86deb49
Daniil Tsarkov <admin@dankantsar.com>
2018-02-12 00:07:25 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+CSRF_ENABLED = True
+SECRET_KEY = 'SuperSecretPassword'
+
+basedir = os.path.abspath(os.path.dirname(__name__))
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.sqlite')
---
+++ migrate.py
@@ -0,0 +1,5 @@
+from web import db
+from web.models import *
+
+db.drop_all()
+db.create_all()
