f8226f8b3f57f519c8b55c98761cc7dc3a841d7a
Paul Dyachek <dyachkep@gmail.com>
2018-02-17 03:50:52 -0500

    added fullscreen mode for calibrate page

--- web/views.py
+++ web/views.py
@@ -5,3 +5,7 @@ from web import app
 @app.route('/', methods=['GET', 'POST'])
 def main():
     return render_template('main.html')
+
+@app.route('/calibrate', methods=['GET', 'POST'])
+def multicheck():
+    return render_template('color.html', color="#FF0000")
