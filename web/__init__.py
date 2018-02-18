8a73928fd355f5f3889e92a34ea88e8b109a707c
Valegov Danila <danilavalegov12gr@gmail.com>
2018-02-18 16:39:25 +0300

    Редактирование структуры

--- web/__init__.py
+++ web/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 
-app = Flask(__name__)
-app.config.from_object('config')
+app = Flask(__name__, static_url_path="/static")
+app.config.from_object("config")
 db = SQLAlchemy(app)
+Bootstrap(app)
 
-import web.models
 import web.views
+
