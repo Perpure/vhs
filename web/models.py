e0ccdd5b0d65bbcb97f545cf13cf355860453498
Konstantin Alekseev <drinor4@gmail.com>
2018-02-19 19:27:08 +0300

    sdelal

--- web/models.py
+++ web/models.py
@@ -6,7 +6,7 @@ from web import db
 class Video(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
-    path = db.Column('src/video')
+    path = db.Column(db.Column(db.Text(), unique=True, nullable=False))
 
 
 
