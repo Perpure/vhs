516a1abb8e62915eb60e4e990ec8e8ee00b142dd
Ivan <kucherovivan45@gmail.com>
2018-02-25 00:06:52 +0300

    video

--- web/views.py
+++ web/views.py
@@ -5,3 +5,7 @@ from web import app
 @app.route('/', methods=['GET', 'POST'])
 def main():
     return render_template('main.html')
+
+@app.route('/video', methods=['GET', 'POST'])
+def video():
+    return render_template('video.html')
