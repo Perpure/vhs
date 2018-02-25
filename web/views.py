262d4820cc3ef4287386cfab40a2efaf08c8c718
shihanov12 <@gmail.com>
2018-02-25 16:09:18 +0300

    upload video work

--- web/views.py
+++ web/views.py
@@ -1,6 +1,8 @@
+from werkzeug.utils import secure_filename
 from flask import redirect, render_template, request, url_for
 from web import app
 from web.forms import UploadVideoForm
+from web import ALLOWED_EXTENSIONS
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -14,21 +16,23 @@ def allowed_file(filename):
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     form = UploadVideoForm(csrf_enabled=False)
-    if form.validate_on_submit():
+    if request.method == 'POST':
+        print('    ')
+        print(request.files)
         print(form.name.data)
-        print(dir(request.files))
-        #if form.name.data not in request.files:
+        print('    ')
+        if 'name' not in request.files:
             #flash('No file part')
-            #return redirect(request.url)
-        file = request.files[form.name.data]
+            print('YES')
+            return redirect(request.url)
+        file = request.files['name']
         print(file)
         if file.filename == '':
             #flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-    
+            file.save('videos/' + filename)
+            return redirect(request.url)
+    #os.path.join(app.config['UPLOAD_FOLDER'], filename)
     return render_template('upload_video.html', form=form)
