d2077ca94345a0b1e9d791afee98728e97c9c2ea
Valegov Danila <danilavalegov12gr@gmail.com>
2018-03-04 16:45:17 +0300

    Вынужденный промежуточный коммит(ничего не работает)

--- web/__init__.py
+++ web/__init__.py
@@ -8,6 +8,7 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'mp4'])
 app = Flask(__name__, static_url_path="/static")
 app.config.from_object("config")
 db = SQLAlchemy(app)
+Bootstrap(app)
 
 import web.views
 
