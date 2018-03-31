bf638a085edb100470e125b32c088d3826c7b41f
Inversion <InversionSpaces@vivaldi.net>
2018-03-31 12:25:44 +0300

    Comments near to be done

--- web/views.py
+++ web/views.py
@@ -306,16 +306,17 @@ def play(vid):
     user = cur_user()
     is_viewed = IsVideoViewed.is_viewed
     form = AddCommentForm(csrf_enabled=False)
+
     if form.validate_on_submit():
         comment = Comment(form.message.data, video.id, user.id)
         comment.save()
-    comments = Comment.query.all
+
     if (video.id not in is_viewed) and (video is not None) and (user is not None):
         IsVideoViewed.is_viewed.append(video.id)
         video.views += 1
         db.session.add(video)
         db.session.commit()
-    return render_template('play.html', user=cur_user(), vid=vid, video=Video.get(vid), video_views=video.views)
+    return render_template('play.html', user=user, vid=vid, video=video, video_views=video.views, form=form)
 
 
 @app.errorhandler(403)
