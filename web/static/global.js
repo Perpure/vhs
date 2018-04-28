b1e0d5e039a0bc7a8becfd8820a1d339e02babb8
Konnov Konstantin <boneyandoom@gmail.com>
2018-04-28 23:11:50 +0300

    footer fix + new menu

--- web/static/global.js
+++ web/static/global.js
@@ -3,3 +3,6 @@ let body=document.getElementById("Body");
 let wid=body.offsetWidth;
 if(wid<=1200)elem.innerHTML="VHS";
 
+let Height=document.body.scrollHeight;
+let foot=document.getElementById("Footer");
+foot.style.top=(Height-130)+"px";
