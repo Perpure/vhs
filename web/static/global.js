59eaa13161c45697508b427126e58d465c9bac1c
Konnov Konstantin <boneyandoom@gmail.com>
2018-04-28 23:52:37 +0300

    all search begin

--- web/static/global.js
+++ web/static/global.js
@@ -6,3 +6,22 @@ if(wid<=1200)elem.innerHTML="VHS";
 let Height=document.body.scrollHeight;
 let foot=document.getElementById("Footer");
 foot.style.top=(Height-130)+"px";
+
+let searching=false;
+let search=document.getElementById("Search");
+let searcher=document.getElementById("searcher");
+let shadow=document.getElementById("Shad");
+search.addEventListener('click',function(){
+    if(searching)
+    {
+        shadow.style.display="none";
+        searcher.style.display="none";
+        searching=false;
+    }
+    else
+    {       
+        shadow.style.display="block";
+        searcher.style.display="block";
+        searching=true;
+    }
+});
