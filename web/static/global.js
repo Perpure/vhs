b9107d9ac8ee8358ca4b6c18b18513fbb004ff97
Konnov Konstantin <boneyandoom@gmail.com>
2018-04-29 13:47:55 +0300

    hotfix 2.0

--- web/static/global.js
+++ web/static/global.js
@@ -1,7 +1,7 @@
 let elem=document.getElementById("logoTxt");
 let body=document.getElementById("Body");
 let wid=body.offsetWidth;
-if(wid<=1200)elem.innerHTML="VHS";
+if(wid<=1400)elem.innerHTML="VHS";
 
 let Height=document.body.scrollHeight;
 let foot=document.getElementById("Footer");
