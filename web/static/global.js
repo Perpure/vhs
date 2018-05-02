6789fb660d0240b32ff624504178f0b0cf3ce24d
Konnov Konstantin <boneyandoom@gmail.com>
2018-05-02 11:40:22 +0300

    mobile menu

--- web/static/global.js
+++ web/static/global.js
@@ -3,9 +3,17 @@ let body=document.getElementById("Body");
 let wid=body.offsetWidth;
 if(wid<=550)elem.innerHTML="VHS";
 
-let Height=document.body.scrollHeight;
-let foot=document.getElementById("Footer");
-foot.style.top=(Height-130)+"px";
+function mov()
+{
+    
+    let Height=document.body.scrollHeight;
+    let foot=document.getElementById("Footer");
+    foot.style.top=0;
+    foot.style.top=(Height-130)+"px";
+    console.log(11);
+}
+
+mov();
 
 let searching=false;
 let search=document.getElementById("Search");
@@ -24,4 +32,25 @@ search.addEventListener('click',function(){
         searcher.style.display="block";
         searching=true;
     }
+});
+
+let holder=document.getElementById("mHolder");
+let opned=false;
+holder.addEventListener('click',function(){
+    let els=document.getElementsByClassName("navMen");
+    if(opned)
+    {        
+        for(let i=0;i<els.length;i++)
+            els[i].style.display="none";
+        holder.style.backgroundColor="rgba(240, 203, 142,0)";
+        opned=false;
+    }
+    else
+    {
+        for(let i=0;i<els.length;i++)
+            els[i].style.display="block";
+        holder.style.backgroundColor="rgba(240, 203, 142,0.4)";
+        opned=true;
+    }
+    mov();
 });
