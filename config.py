79a9607b59d0945417c530af5b967e6e094410e8
John Syomochkin <info@syomochkin.xyz>
2018-06-26 22:24:33 +0300

    #11 бд определяается через DATABASE_URL, подготовил файл-пример перменных среды, описал порядок инициализации БД в README

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'SuperSecretPassword'
 
 basedir = os.path.abspath(os.path.dirname(__name__))
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.sqlite')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 UPLOADS_DEFAULT_DEST = os.path.join(basedir, 'uploads')
 VIDEO_SAVE_PATH = os.path.join(basedir, 'video')
 ALLOWED_EXTENSIONS = {'mp4', 'ogv', 'mpeg', 'avi', 'mov', 'webm', 'flv'}
