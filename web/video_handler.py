c80281d55a8ae29ea39a54c43db6c29dcaca63eb
Daniil Tsarkov <dankan_tsar@protonmail.ch>
2018-06-30 21:15:06 +0300

    multiformat coding

--- web/video_handler.py
+++ web/video_handler.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from random import random
 from hashlib import md5
+import threading
 from os import makedirs
 from os.path import join as join_path
 from moviepy.editor import VideoFileClip
@@ -9,12 +10,28 @@ from web.models import Video
 from web.helper import cur_user
 
 
-def standardize_video_extension(video_id, ext):
+def standardize_video_extension_mp4(video_id, ext):
     video = Video.get(video_id=video_id)
     video_path = join_path(video.path, 'video.' + ext)
 
     video_clip = VideoFileClip(video_path)
     video_clip.write_videofile(join_path(video.path, 'video.mp4'))
+
+
+def standardize_video_extension_ogv(video_id, ext):
+    video = Video.get(video_id=video_id)
+    video_path = join_path(video.path, 'video.' + ext)
+
+    video_clip = VideoFileClip(video_path)
+    video_clip.write_videofile(join_path(video.path, 'video.ogv'))
+
+
+def standardize_video_extension_webm(video_id, ext):
+    video = Video.get(video_id=video_id)
+    video_path = join_path(video.path, 'video.' + ext)
+
+    video_clip = VideoFileClip(video_path)
+    video_clip.write_videofile(join_path(video.path, 'video.webm'), codec='libvpx')
 
 
 def create_preview(video_id, ext):
@@ -38,8 +55,11 @@ def save_video(video_file, title):
     makedirs(directory)
     video_path = join_path(directory, 'video.' + ext)
     video_file.save(video_path)
+    create_preview(video.id, ext)
     try:
-        prepare_video(video.id, ext)
+        prepare_thread = threading.Thread(target=prepare_video, args=(video.id, ext))
+        prepare_thread.start()
+        # prepare_video(video.id, ext)
     except OSError:
         video.delete_video()
         return None
@@ -47,7 +67,7 @@ def save_video(video_file, title):
 
 
 def prepare_video(video_id, ext):
-    create_preview(video_id, ext)
-
     if ext != 'mp4':
-        standardize_video_extension(video_id, ext)
+        standardize_video_extension_mp4(video_id, ext)
+    standardize_video_extension_ogv(video_id, ext)
+    standardize_video_extension_webm(video_id, ext)
