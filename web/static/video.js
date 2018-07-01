16f51f0f524dcf7e0c6c35cdde34056d01005600
Konnov Konstantin <boneyandoom@gmail.com>
2018-07-01 20:50:25 +0300

    #21 refact

--- web/static/video.js
+++ web/static/video.js
@@ -24,7 +24,12 @@ setTimeout(function step(){
                     success:function(response1)
                     {
                         response1.forEach(function(element){
-                            $("#CSect").get(0).innerHTML='<div class="comment"><div class="comment_ava"><img src="'+element.ava+'" alt="" class="comment_ava-img"></div><div class="comment_txt"><p class="comment_txt__auth"><a href="/cabinet/'+element.login+'">'+element.name+'</a></p><p>'+element.text+'</p></div></div>'+$("#CSect").get(0).innerHTML;
+                          var comment=$('<div class="comment"></div>');
+                          var ava=$('<div class="comment_ava"><img src="'+element.ava+'" alt="" class="comment_ava-img"></div>');
+                          var txt=$('<div><p class="comment_author"><a href="/cabinet/'+element.login+'">'+element.name+'</a></p><p>'+element.text+'</p></div></div>');
+                          comment.append(ava);
+                          comment.append(txt);
+                          $('#CSect').prepend(comment);
                         });
                     },
                     error:function(){}
