4661ab1f7bf4806401ee729a4179c34233f37405
John Syomochkin <info@syomochkin.xyz>
2018-07-12 00:17:03 +0300

    #20 поправил ci, обновил клиентскую часть поиска видео через youtube

--- web/static/js/multiscreen/youtube_search.js
+++ web/static/js/multiscreen/youtube_search.js
@@ -1,96 +1,84 @@
-var videos = [];
-var next_page_token = '';
-var more_videos = '<button class="yt-search__more" id="more_videos">Больше видео</button>';
-var search;
-
+var YTSearchQuery = '',
+    YTSearchNextPageToken = '';
 
 jQuery(function($) {
 
-function find_videos() {
-    search = $('#search').val();
-    $('#videos_block').html('');
-    $('.yt-video_error').remove();
+    function find_videos(query) {
+        YTSearchQuery = query;
+        $('.yt-search__result').html('');
+        $('.yt-video_error').remove();
+        $('.yt-search__more').addClass('yt-search__more_hidden');
 
-    if (search.length > 0) {
+        if (query.length > 0) {
+            send_video_search(query);
+        } else {
+            $('.yt-search__searcher').after('<p class="yt-video_error">Строка запроса не должна быть пустой</p>');
+        }
+    }
 
+    function get_more_videos(query, nextPageToken) {
+        $('.yt-search__more').addClass('yt-search__more_hidden');
+        send_video_search(query, nextPageToken);
+    }
+
+    function send_video_search(query, nextPageToken) {
+        var requestData = {
+                query: query
+            };
+        if (nextPageToken) {
+            requestData.nextPageToken = nextPageToken;
+        }
         $.ajax({
-            url:'/youtube_videos',
-            data: {'query': search}
+            url: '/youtube_videos',
+            data: requestData
         }).done(function(data) {
-            handle_data(data)
+            handle_data(data);
         }).fail(function(jqXHR) {
             if (jqXHR.status == 404) {
-                $('#searcher_block').after('<p class="yt-video_error">Видео не найдены</p>')
+                $('.yt-search__searcher').after('<p class="yt-video_error">Видео не найдены</p>');
             }
             else if (jqXHR.status == 500) {
-                $('#searcher_block').after('<p class="yt-video_error">Ошибка работы сервера</p>')
+                $('.yt-search__searcher').after('<p class="yt-video_error">Ошибка работы сервера</p>');
             }
             else {
-                $('#searcher_block').after('<p class="yt-video_error">Неизвестная ошибка</p>')
+                $('.yt-search__searcher').after('<p class="yt-video_error">Неизвестная ошибка</p>');
             }
-
         });
-
-    } else {
-        $('#searcher_block').after('<p class="yt-video_error">Строка запроса не должна быть пустой</p>')
     }
-}
 
+    function handle_data(data) {
+        var videos = data.videos;
+        YTSearchNextPageToken = data.nextPageToken;
+        for (var i = 0; i < videos.length; i++) {
+            output_video(videos[i]);
+        }
+        if (videos.length && YTSearchNextPageToken) {
+            $('.yt-search__more').removeClass('yt-search__more_hidden');
+        }
+    }
 
-function get_more_videos() {
-    $('#more_videos').remove();
+    function output_video(video_item) {
+        var html = '<div class="video">' +
+                        '<div class="video_preview">' +
+                            '<img class="video_preview-img" src="' + video_item.preview + '">' +
+                        '</div>' +
+                        '<div>' +
+                            '<p class="video_title">' + video_item.title + '</p>' +
+                            '<p class="italic">' + video_item.author + '<br>' + video_item.duration + '</p>' +
+                            '<span hidden="true">' + video_item.id + '</span>' +
+                        '</div>' +
+                    '</div>';
+        $('.yt-search__result').append(html);
+    }
 
-    $.ajax({
-        url:'/youtube_videos',
-        data: {
-            'query': search,
-            'nextPageToken': next_page_token
-        }
-    }).done(function(data) {
-        handle_data(data);
-    }).fail(function(jqXHR) {
-        if (jqXHR.status == 404) {
-            $('#videos_block').after('<p class="yt-video_error">Видео не найдены</p>')
-        }
-        else if (jqXHR.status == 500) {
-            $('#videos_block').after('<p class="yt-video_error">Ошибка работы сервера</p>')
-        }
-        else {
-            $('#videos_block').after('<p class="yt-video_error">Неизвестная ошибка</p>')
-        }
+    $('.yt-searcher__submit button').click(function() {
+        var ytSearcher = $(this).parents('.yt-searcher'),
+            ytSearcherInput = ytSearcher.find('.yt-searcher__input input'),
+            query = ytSearcherInput.val();
+        find_videos(query);
     });
-}
 
-
-function handle_data(data) {
-    next_page_token = data.nextPageToken;
-    videos = data.videos;
-    for (var id in videos) {
-        output_video(videos[id]);
-    }
-    if (next_page_token != 0 && videos.length > 0) {
-        $('#videos_block').append(more_videos);
-        $('#more_videos').click(get_more_videos);
-    }
-}
-
-
-function output_video(video_item) {
-    var html = '<div class="video">' +
-                    '<div class="video_preview">' +
-                    '<img class="video_preview-img" src="' + video_item.preview + '">' +
-                    '</div>' +
-                    '<div>' +
-                    '<p class="video_title">' + video_item.title + '</p>' +
-                    '<p class="italic">' + video_item.author + '<br>' + video_item.duration + '</p>' +
-                    '<span hidden="true">' + video_item.id + '</span>' +
-                    '</div>' +
-                '</div>';
-    $('#videos_block').append(html);
-}
-
-$(document).ready(function() {
-    $('#submit').click(find_videos);
-});
-
+    $('.yt-search__more').click(function() {
+        get_more_videos(YTSearchQuery, YTSearchNextPageToken);
+    });
 });
