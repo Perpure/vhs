aa34f9beec322b10dbc9675ab0f0974f6bf54496
Konnov Konstantin <boneyandoom@gmail.com>
2018-07-12 01:43:11 +0300

    #23 standart tabs & fix comments

--- web/static/js/video_page/video.js
+++ web/static/js/video_page/video.js
@@ -5,53 +5,23 @@ for(var i=0;i<hash.length;i++)
     if(hash[i]=='/')sl=i;
 hash=hash.substring(sl+1);
 
-var curComms = document.getElementsByClassName("comment").length;
-setTimeout(function step(){
+if($('#addC')!=null)$('#addC').click(function(){
+    var txt=$('#txtPlate').val();
     $.ajax({
-        url:"/askNewComm/"+hash,
+        url:"/postComm/"+hash+"/",
         type:"GET",
         dataType:"text",
-        success:function(response)
-        {
-            if(response>curComms)
-            {
-                var def=response-curComms;
-                $.ajax({
-                    url:"/getNewComm/"+hash+"/"+curComms,
-                    type:"GET",
-                    dataType:"json",
-                    success:function(response1)
-                    {
-                        response1.forEach(function(element){
-                          var comment=$('<div class="comment"></div>');
-                          var ava=$('<div class="comment_ava"><img src="'+element.ava+'" alt="" class="comment_ava-img"></div>');
-                          var txt=$('<div><p class="comment_author"><a href="/cabinet/'+element.login+'">'+element.name+'</a></p><p>'+element.text+'</p></div></div>');
-                          comment.append(ava);
-                          comment.append(txt);
-                          $('#CSect').prepend(comment);
-                        });
-                    },
-                    error:function(){}
-                });
-                curComms=response;
-            }
-        },
-        error:function(){}
+        success:function(response){},
+        error:function(){},
+        data:{comm: txt}
     });
-    setTimeout(step,speed);
-},speed);
-
-if($('#addC')!=null)$('#addC').click(function(){
-            var txt=$('#txtPlate').val();
-            $.ajax({
-               url:"/postComm/"+hash+"/",
-               type:"GET",
-               dataType:"text",
-               success:function(response){},
-               error:function(){},
-               data:{comm: txt}
-            });
-            plate.value="";
+    $('#txtPlate').val('');
+    var comment=$('<div class="comment"></div>');
+    var ava=$('<div class="comment_ava"><img src="'+$('#myAva').attr('src')+'" alt="" class="comment_ava-img"></div>');
+    var txt=$('<div><p class="comment_author"><a href="'+$('#myProf').attr('href')+'">'+$('#myProf').html()+'</a></p><p>'+txt+'</p></div></div>');
+    comment.append(ava);
+    comment.append(txt);
+    $('#CSect').prepend(comment);
 });
 
 function calc()
@@ -65,9 +35,9 @@ function calc()
     denom=lkes*1+disl*1;
     if(!denom)
     {
-      lkes=0.5;
-      disl=0.5;
-      denom=1;
+        lkes=0.5;
+        disl=0.5;
+        denom=1;
     }
     lkesIn.css('width',(lkes*100)/denom+"%");
     disIn.css('width',(disl*100)/denom+"%");
@@ -76,41 +46,40 @@ function calc()
 calc();
 
 $('.tag').click(function(){
-        var val = this.innerText;
-        var vw=0;
-        var dt=0;
-        if (pr_page == "" ) {
-            map_needed=$('#show_video_map').prop('checked');
-        }
+    var val = this.innerText;
+    var vw=0;
+    var dt=0;
+    if (pr_page == "" ) {
+        map_needed=$('#show_video_map').prop('checked');
+    }
 
-        if(val=="") val=" ";
-        $.ajax({
-                       url:"/startSearch",
-                       type:"GET",
-                       dataType:"html",
-                       data: {
-                            ask:val,
-                            view:vw,
-                            dat:dt
-                       },
-                       success:function(response)
-                       {
-                         var placer="";
-                         var plus=false;
-                         response=$.parseHTML(response);
-                         var tempDom = $('search').append(response);
-                         var maine=$('#Main', tempDom);
-                         $(tempDom).empty();
-                         $("#Main").html($(maine).html());
-                       },
-                       error:function(){}
-        });
-        if (pr_page != "") {
-            $.getScript("https://api-maps.yandex.ru/2.1/?lang=ru_RU", function () {$.getScript('/static/videos_map.js');});
-        }
-        else {
-            $.getScript('/static/videos_map.js');
-        }
-        pr_page="";
-
+    if(val=="") val=" ";
+    $.ajax({
+        url:"/startSearch",
+        type:"GET",
+        dataType:"html",
+        data: {
+            ask:val,
+            view:vw,
+            dat:dt
+        },
+        success:function(response)
+        {
+            var placer="";
+            var plus=false;
+            response=$.parseHTML(response);
+            var tempDom = $('search').append(response);
+            var maine=$('#Main', tempDom);
+            $(tempDom).empty();
+            $("#Main").html($(maine).html());
+        },
+        error:function(){}
     });
+    if (pr_page != "") {
+        $.getScript("https://api-maps.yandex.ru/2.1/?lang=ru_RU", function () {$.getScript('/static/videos_map.js');});
+    }
+    else {
+        $.getScript('/static/videos_map.js');
+    }
+    pr_page="";
+});
--- web/static/js/video_page/video_like.js
+++ web/static/js/video_page/video_like.js
@@ -3,69 +3,73 @@ var dlk=$("#dislike");
 var condition=0;
 
 if(lk.css('background-image')[lk.css('background-image').length-7]=='L')
-  condition=1;
+{
+    condition=1;
+}
 if(dlk.css('background-image')[dlk.css('background-image').length-7]=='D')
-  condition=-1;
+{
+    condition=-1;
+}
 
 $('#like').click(function(){
-            $.ajax({
-               url:"/likeVideo/"+hash,
-               type:"GET",
-               dataType:"text",
-               success:function(response){},
-               error:function(){
-                    alert('Произошла ошибка')}
-            });
-            if(condition==1)
-            {
-                lk.css('background-image',"url(/static/images/lik1.png)");
-                $("#lik").html($("#lik").html()-1);
-                condition=0;
-            }
-            else if(condition){
-                lk.css('background-image',"url(/static/images/lik1L.png)");
-                dlk.css('background-image',"url(/static/images/dis1.png)");
-                condition=1;
-                $("#lik").html($("#lik").html()-(-1));
-                $("#dis").html($("#dis").html()-1);
-            }
-            else
-            {
-              lk.css('background-image',"url(/static/images/lik1L.png)");
-              condition=1;
-              $("#lik").html($("#lik").html()-(-1));
-            }
-            calc();
+    $.ajax({
+        url:"/likeVideo/"+hash,
+        type:"GET",
+        dataType:"text",
+        success:function(response){},
+        error:function(){
+            alert('Произошла ошибка')}
+    });
+    if(condition==1)
+    {
+        lk.css('background-image',"url(/static/images/lik1.png)");
+        $("#lik").html($("#lik").html()-1);
+        condition=0;
+    }
+    else if(condition){
+        lk.css('background-image',"url(/static/images/lik1L.png)");
+        dlk.css('background-image',"url(/static/images/dis1.png)");
+        condition=1;
+        $("#lik").html($("#lik").html()-(-1));
+        $("#dis").html($("#dis").html()-1);
+    }
+    else
+    {
+        lk.css('background-image',"url(/static/images/lik1L.png)");
+        condition=1;
+        $("#lik").html($("#lik").html()-(-1));
+    }
+    calc();
 });
 
 $('#dislike').click(function(){
-            $.ajax({
-               url:"/dislikeVideo/"+hash,
-               type:"GET",
-               dataType:"text",
-               success:function(response){},
-               error:function(){
-                    alert('Произошла ошибка')}
-            });
-            if(condition==-1)
-            {
-                dlk.css('background-image',"url(/static/images/dis1.png)");
-                condition=0;
-                $("#dis").html($("#dis").html()-1);
-            }
-            else if(condition)
-            {
-                dlk.css('background-image',"url(/static/images/dis1D.png)");
-                lk.css('background-image',"url(/static/images/lik1.png)");
-                condition=-1;
-                $("#dis").html($("#dis").html()-(-1));
-                $("#lik").html($("#lik").html()-1);
-            }
-            else
-            {
-                dlk.css('background-image',"url(/static/images/dis1D.png)");
-                condition=-1;
-                $("#dis").html($("#dis").html()-(-1));
-            }
-            calc();
+    $.ajax({
+        url:"/dislikeVideo/"+hash,
+        type:"GET",
+        dataType:"text",
+        success:function(response){},
+        error:function(){
+            alert('Произошла ошибка')}
+    });
+    if(condition==-1)
+    {
+        dlk.css('background-image',"url(/static/images/dis1.png)");
+        condition=0;
+        $("#dis").html($("#dis").html()-1);
+    }
+    else if(condition)
+    {
+        dlk.css('background-image',"url(/static/images/dis1D.png)");
+        lk.css('background-image',"url(/static/images/lik1.png)");
+        condition=-1;
+        $("#dis").html($("#dis").html()-(-1));
+        $("#lik").html($("#lik").html()-1);
+    }
+    else
+    {
+        dlk.css('background-image',"url(/static/images/dis1D.png)");
+        condition=-1;
+        $("#dis").html($("#dis").html()-(-1));
+    }
+    calc();
 });
