3b4e0107d1bdd84bd7124bd27146f6940091115c
Paul Dyachek <dyachekp@gmail.com>
2018-07-12 14:40:49 +0300

    18 issue resolved

--- web/parser.py
+++ web/parser.py
@@ -71,6 +71,7 @@ def save_map(draw, room, room_map):
 
 
 def parse(room, devices, impath):
+    is_parsed = False
     img = cv2.imread(impath)
     hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     items = list()
@@ -87,11 +88,19 @@ def parse(room, devices, impath):
         hue_max = np.array([min(hue + 10, 179), 255, 255], dtype=np.uint8)
         thresh = cv2.inRange(hsv_img, hue_min, hue_max)
         _, contours, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        rect = cv2.minAreaRect(sorted(contours, key=cv2.contourArea, reverse=True)[0])
-        box = np.int0(cv2.boxPoints(rect))
-        minX = min(minX, np.ndarray.min(box[..., 0]))
-        maxX = max(maxX, np.ndarray.max(box[..., 0]))
-        minY = min(minY, np.ndarray.min(box[..., 1]))
-        maxY = max(maxY, np.ndarray.max(box[..., 1]))
-        items.append([device, rect, (color[2], color[1], color[0])])
-    handle_parse(items, minX, minY, maxX, maxY, room)
+        try:
+            rect = cv2.minAreaRect(sorted(contours, key=cv2.contourArea, reverse=True)[0])
+            box = np.int0(cv2.boxPoints(rect))
+            minX = min(minX, np.ndarray.min(box[..., 0]))
+            maxX = max(maxX, np.ndarray.max(box[..., 0]))
+            minY = min(minY, np.ndarray.min(box[..., 1]))
+            maxY = max(maxY, np.ndarray.max(box[..., 1]))
+            items.append([device, rect, (color[2], color[1], color[0])])
+            is_parsed = True
+        except IndexError:
+            pass
+    if is_parsed:
+        handle_parse(items, minX, minY, maxX, maxY, room)
+        return True
+    else:
+        return False
