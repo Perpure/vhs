f85e83af807de234654ef964d5fa35df14aea719
Paul Dyachek <dyachekp@gmail.com>
2018-07-12 12:44:17 +0300

    1000th refactor commit

--- web/parser.py
+++ web/parser.py
@@ -8,32 +8,47 @@ from PIL import Image, ImageDraw
 
 
 class Screen():
-    def __init__(self, width, height, deltax = 0, deltay = 0):
+    def __init__(self, width, height, left = 0, top = 0):
         self.width = width
         self.height = height
-        self.deltax = deltax
-        self.deltay = deltay
+        self.left = left
+        self.top = top
 
-    def get_formatted_screen(self, w, h):
+    def get_formatted_screen(self, picture_size):
         width = self.width
         height = self.height
-        if (width / height) > (w / h):
-            height = int(width * h / w)
-        elif (width / height) < (w / h):
-            width = int(height * w / h)
-        deltax = (width - self.width) // 2
-        deltay = (height - self.height) // 2
-        return Screen(width, height, deltax, deltay)
+        if (width / height) > (picture_size):
+            height = int(width / picture_size)
+        elif (width / height) < (picture_size):
+            width = int(height * picture_size)
+        left = - (width - self.width) // 2
+        top = - (height - self.height) // 2
+        return Screen(width, height, left, top)
+
+    def get_device_screen(self, device, rect):
+        if -95 < rect[2] < -85:
+            firsty = int(rect[0][1] - rect[1][0] / 2) - self.top
+            firstx = int(rect[0][0] - rect[1][1] / 2) - self.left
+            lastx = int(rect[0][0] + rect[1][1] / 2) - self.left
+        else:
+            firsty = int(rect[0][1] - rect[1][1] / 2) - self.top
+            firstx = int(rect[0][0] - rect[1][0] / 2) - self.left
+            lastx = int(rect[0][0] + rect[1][0] / 2) - self.left
+        width = (self.width / (lastx - firstx)) * 100
+        left = - (firstx / self.width) * width
+        top = - (firsty / self.height) * width
+        return Screen(width, None, left, top)
 
 
 def handle_parse(items, minX, minY, maxX, maxY, room):
     trimmed_screen = Screen(maxX - minX, maxY - minY)
-    final_screen = trimmed_screen.get_formatted_screen(16, 9)
+    final_screen = trimmed_screen.get_formatted_screen(16 / 9)
     draw, room_map = create_map(final_screen.width, final_screen.height)
     for item in items:
-        user, rect, color = item
+        device, rect, color = item
         rect = ((rect[0][0] - minX, rect[0][1] - minY), rect[1], rect[2])
-        save_parse(user, rect, final_screen.deltax, final_screen.deltay, final_screen.width, final_screen.height)
+        device_screen = final_screen.get_device_screen(device, rect)
+        device.save_screen_params(device_screen)
         draw_map(draw, rect, color)
     save_map(draw, room, room_map)
 
@@ -55,40 +70,22 @@ def save_map(draw, room, room_map):
     room_map.save(filename)
 
 
-def save_parse(user, rect, deltax, deltay, new_width, new_height):
-    if -95 < rect[2] < -85:
-        firsty = int(rect[0][1] - rect[1][0] / 2) + deltay
-        firstx = int(rect[0][0] - rect[1][1] / 2) + deltax
-        lastx = int(rect[0][0] + rect[1][1] / 2) + deltax
-    else:
-        firsty = int(rect[0][1] - rect[1][1] / 2) + deltay
-        firstx = int(rect[0][0] - rect[1][0] / 2) + deltax
-        lastx = int(rect[0][0] + rect[1][0] / 2) + deltax
-    width = (new_width / (lastx - firstx)) * 100
-    left = - (firstx / new_width) * width
-    top = - (firsty / new_height) * width
-    user.res_k = int(width)
-    user.top = int(top)
-    user.left = int(left)
-    db.session.commit()
-
-
-def parse(room, users, impath):
+def parse(room, devices, impath):
     img = cv2.imread(impath)
     hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     items = list()
     maxX = maxY = -math.inf
     minY = minX = math.inf
-    for user in users:
-        R = int(user.color[1:3], 16)
-        G = int(user.color[3:5], 16)
-        B = int(user.color[5:7], 16)
+    for device in devices:
+        R = int(device.color[1:3], 16)
+        G = int(device.color[3:5], 16)
+        B = int(device.color[5:7], 16)
         color = (B, G, R)
         hsv_color = np.array(color, dtype=np.uint8, ndmin=3)
         hue = cv2.cvtColor(hsv_color, cv2.COLOR_BGR2HSV).flatten()[0]
-        h_min = np.array([max(hue - 10, 0), 100, 100], dtype=np.uint8)
-        h_max = np.array([min(hue + 10, 179), 255, 255], dtype=np.uint8)
-        thresh = cv2.inRange(hsv_img, h_min, h_max)
+        hue_min = np.array([max(hue - 10, 0), 100, 100], dtype=np.uint8)
+        hue_max = np.array([min(hue + 10, 179), 255, 255], dtype=np.uint8)
+        thresh = cv2.inRange(hsv_img, hue_min, hue_max)
         _, contours, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         rect = cv2.minAreaRect(sorted(contours, key=cv2.contourArea, reverse=True)[0])
         box = np.int0(cv2.boxPoints(rect))
@@ -96,5 +93,5 @@ def parse(room, users, impath):
         maxX = max(maxX, np.ndarray.max(box[..., 0]))
         minY = min(minY, np.ndarray.min(box[..., 1]))
         maxY = max(maxY, np.ndarray.max(box[..., 1]))
-        items.append([user, rect, (color[2], color[1], color[0])])
+        items.append([device, rect, (color[2], color[1], color[0])])
     handle_parse(items, minX, minY, maxX, maxY, room)
--- web/websocket.py
+++ web/websocket.py
@@ -37,5 +37,5 @@ def multiscreen_show_result(message):
             noSound = True
             if member == users[0]:
                 noSound = False
-            params = {'top': member.top, 'left': member.left, 'scale': member.res_k, 'noSound': noSound}
+            params = {'top': member.top, 'left': member.left, 'scale': member.scale, 'noSound': noSound}
             emit('multiscreen_show_result', params, room=member.socket_id)
