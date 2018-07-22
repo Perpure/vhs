0c047f275cfb68031d3fb0b49b13f59d5d2652a5
Daniil Tsarkov <dankan_tsar@protonmail.ch>
2018-07-22 21:13:30 +0300

    Task #28 static reload on change

--- web/helper.py
+++ web/helper.py
@@ -9,6 +9,20 @@ from config import basedir
 from web.parser import parse
 
 
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
+
 def requiresauth(f):
     @wraps(f)
     def wrapped(*args, **kwargs):
