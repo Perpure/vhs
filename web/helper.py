12d29ab22ac7fa6550f14d235a7b9c838a3a576a
Daniil Tsarkov <dankan_tsar@protonmail.ch>
2018-07-22 22:11:27 +0300

    Task #28 final ci fix

--- web/helper.py
+++ web/helper.py
@@ -13,6 +13,7 @@ from web.parser import parse
 def override_url_for():
     return dict(url_for=dated_url_for)
 
+
 def dated_url_for(endpoint, **values):
     if endpoint == 'static':
         filename = values.get('filename', None)
