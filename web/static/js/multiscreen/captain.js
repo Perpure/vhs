3e8b7c1bbac6ebd023e292b169e8fcfe32a88626
Konnov Konstantin <boneyandoom@gmail.com>
2018-08-18 13:38:48 +0300

    #42 new room ready

--- web/static/js/multiscreen/captain.js
+++ web/static/js/multiscreen/captain.js
@@ -152,11 +152,5 @@ $('#refresh_btn').click(function() {
   drop_state();
 });
 
-    var PageTour;
-    try {
-        PageTour = $.fn.PageTour();
-    } catch (e) {
-        console.error('Cannot start page tour: ', e);
-    }
-    $('#startTour').click(PageTour.open);
+    const driver = new Driver();
 });
