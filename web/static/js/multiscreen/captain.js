c04b060ba23bc0b1254c4bc556e3aa0fb032f744
Konnov Konstantin <boneyandoom@gmail.com>
2018-08-19 14:50:35 +0300

    #42 add auto scroll

--- web/static/js/multiscreen/captain.js
+++ web/static/js/multiscreen/captain.js
@@ -1,13 +1,13 @@
 var device = 0;
 if(screen.width<=1024) device++;
 if(screen.width<=500) device++;
-var X = [[-90, 0, 0, -90, -95, -30, 0, -300],
-         [0, 0, 0, 0, 0, 0, 0, -100],
-         [-90, 0, 0, -70, -95, -20, 0, -110]];
+var X = [[-90, 0, 0, -90, -95, -30, 0, -300, 0],
+         [0, 0, 0, 0, 0, 0, 0, -100, 0],
+         [-90, 0, 0, -70, -95, -20, 0, -110, 0]];
 
-var Y = [[60, 40, -40, 40, 50, 45, -35, -40],
-         [60, 40, -40, 40, 50, 45, -35, -40],
-         [60, 40, -40, -60, 50, 45, -35, -90]];
+var Y = [[60, 40, -40, 40, 50, 45, -35, -40, 290],
+         [60, 40, -40, 40, 50, 45, -35, -40, 290],
+         [60, 40, -40, -60, 50, 45, -55, -90, -100]];
 
 jQuery(function($) {
   var socketUrl = location.protocol + '//' + location.host;
@@ -163,6 +163,27 @@ $('#refresh_btn').click(function() {
   drop_state();
 });
 
+    function guideScroll() {
+      setTimeout(function() {
+        var duration = 400;
+        var curTop = $('.jquery-guide-content').offset().top;
+        var height = $('.jquery-guide-content').height();
+        var windLower = $(window).scrollTop();
+        var windUpper = windLower + screen.height;
+        var delta = 0;
+
+        if(curTop + height > windUpper)
+        {
+            delta = curTop + height - windUpper + 20;
+        }
+        else if (curTop < windLower) {
+            var delta = curTop - windUpper - 20;
+        }
+
+        $('body,html').animate({scrollTop: windLower + delta}, duration);
+      }, 420);
+    }
+
     function guide() {
         var i = 0;
         var j = 0;
@@ -172,51 +193,66 @@ $('#refresh_btn').click(function() {
                     element: $('#startTour'),
                     content: '<p>Добро пожаловать в гид по Сплитскрину,<br> нажмите на экран, чтобы продолжить</p>',
                     offsetX: X[device][i++],
-                    offsetY: Y[device][j++]
+                    offsetY: Y[device][j++],
+                    beforeFunc: guideScroll
                 },
                 {
                     element: $('#tourCount'),
                     content: '<p>Пусть ваши друзья зайдут в комнату</p>',
                     offsetX: X[device][i++],
-                    offsetY: Y[device][j++]
+                    offsetY: Y[device][j++],
+                    beforeFunc: guideScroll
                 },
                 {
                     element: $('#tourChoose'),
                     content: '<p>Выберите видео</p>',
                     offsetX: X[device][i++],
-                    offsetY: Y[device][j++]
+                    offsetY: Y[device][j++],
+                    beforeFunc: guideScroll
                 },
                 {
                     element: $('#video_switcher'),
                     content: '<p>Либо с нашего сайта, либо с YouTube</p>',
                     offsetX: X[device][i++],
-                    offsetY: Y[device][j++]
+                    offsetY: Y[device][j++],
+                    beforeFunc: guideScroll
                 },
                 {
                     element: $('#calibrate_btn'),
                     content: '<p>Составьте большой экран из устройств участников</p>',
                     offsetX: X[device][i++],
-                    offsetY: Y[device][j++]
+                    offsetY: Y[device][j++],
+                    beforeFunc: guideScroll
                 },
                 {
                     element: $('#calibrate_btn'),
                     content: '<p>Начните калибровку</p>',
                     offsetX: X[device][i++],
-                    offsetY: Y[device][j++]
+                    offsetY: Y[device][j++],
+                    beforeFunc: guideScroll
                 },
                 {
                     element: $('#tourPhoto'),
                     content: '<p>Сфотографируйте устройства, инициализируйте фотографию</p>',
                     offsetX: X[device][i++],
-                    offsetY: Y[device][j++]
+                    offsetY: Y[device][j++],
+                    beforeFunc: guideScroll
                 },
                 {
                     element: $('#show_res'),
                     content: '<p>Если вас устраивает получившаяся карта устройств, нажмите play для начала демонстрации</p>',
+                    offsetX: X[device][i++],
+                    offsetY: Y[device][j++],
+                    beforeFunc: guideScroll
+                },
+                {
+                    element: $('#tourFinale'),
+                    content: '<p>Надеемся, что вам понравится Сплитскрин, приятного использования</p>',
                     offsetX: X[device][i],
                     offsetY: Y[device][j],
                     isBeforeFuncExec: true,
                     beforeFunc: function(g) {
+                        guideScroll();
                         $('#show_res').slideDown(function() {
                             g.execAction();
                         });
