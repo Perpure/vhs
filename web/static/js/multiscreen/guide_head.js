158295b5f8eab2ad12e26fab5ee113a40662444c
Konnov Konstantin <boneyandoom@gmail.com>
2018-08-28 22:52:56 +0300

    #42 some refactor

--- web/static/js/multiscreen/guide_head.js
+++ web/static/js/multiscreen/guide_head.js
@@ -1,20 +1,24 @@
-var device = 0;
-if(screen.width<=1024)
-{
-    device++;
-}
+var device;
 if(screen.width<=500)
 {
-    device++;
+    device = 'phone';
+} else if(screen.width<=1024)
+{
+    device = 'tablet';
+} else
+{
+    device = 'desktop';
 }
 
-var X = [[-90, 0, 0, -90, -95, -30, 0, -300, 0],
-         [0, 0, 0, 0, 0, 0, 0, -100, 0],
-         [-90, 0, 0, -70, -95, -20, 0, -110, 0]];
+var X = {
+        desktop: [-90, -5, 0, -90, -135, -5, -5, -5, -300, -5, -5, 0],
+        tablet: [0, 0, 0, 0, 0, 0, 0, 0, -130, 0, 0, 0],
+        phone: [-90, 0, 0, -70, -95, -90, 0, -10, -110, -80, -50, 0]};
 
-var Y = [[60, 40, -40, 40, 50, 45, -35, -40, 290],
-         [60, 40, -40, 40, 50, 45, -35, -40, 290],
-         [60, 40, -40, -60, 50, 45, -55, -90, -100]];
+var Y = {
+        desktop: [60, 40, -40, 40, 50, 45, -35, 45, -40, -40, -40, 290],
+        tablet: [60, 40, -40, 40, 50, 45, -35, 45, 45, -40, -40, 290],
+        phone: [60, 40, -40, -60, 50, 45, -35, 45, -105, 45, 45, -100]};
 
 jQuery(function($) {
     function guideScroll() {
@@ -63,7 +67,7 @@ jQuery(function($) {
         },
         {
             element: $('#tourCount'),
-            content: '<p>Пусть ваши друзья зайдут в комнату</p>',
+            content: '<p>Пусть устройства, с которых планируется показ, зайдут в комнату</p>',
             offsetX: X[device][i++],
             offsetY: Y[device][j++],
             beforeFunc: guideScroll
@@ -91,28 +95,49 @@ jQuery(function($) {
         },
         {
             element: $('#calibrate_btn'),
-            content: '<p>Начните калибровку</p>',
+            content: '<p>Начните калибровку, чтобы придать каждому девайсу уникальный цвет</p>',
             offsetX: X[device][i++],
             offsetY: Y[device][j++],
             beforeFunc: guideScroll
         },
         {
             element: $('#tourPhoto'),
-            content: '<p>Сфотографируйте устройства, инициализируйте фотографию</p>',
+            content: '<p>Сфотографируйте устройства</p>',
+            offsetX: X[device][i++],
+            offsetY: Y[device][j++],
+            beforeFunc: guideScroll
+        },
+        {
+            element: $('#submit'),
+            content: '<p>Инициализируйте фотографию, тогда сервер сможет определить положение каждого экрана</p>',
             offsetX: X[device][i++],
             offsetY: Y[device][j++],
             beforeFunc: guideScroll
         },
         {
             element: $('#show_res'),
-            content: '<p>Если вас устраивает получившаяся карта устройств, нажмите play для начала демонстрации</p>',
+            content: '<p>Если карта устройств соответствует их реальному положению и соотношению размеров, нажмите play для начала демонстрации</p>',
+            offsetX: X[device][i++],
+            offsetY: Y[device][j++],
+            beforeFunc: guideScroll
+        },
+        {
+            element: $('#stop_res'),
+            content: '<p>Нажав на эту кнопку, вы остановите показ видео</p>',
+            offsetX: X[device][i++],
+            offsetY: Y[device][j++],
+            beforeFunc: guideScroll
+        },
+        {
+            element: $('#refresh_btn'),
+            content: '<p>Если вы сменили видео, обязательно нажмите эту кнопку</p>',
             offsetX: X[device][i++],
             offsetY: Y[device][j++],
             beforeFunc: guideScroll
         },
         {
             element: $('#tourFinale'),
-            content: '<p>Надеемся, что вам понравится Сплитскрин, приятного использования</p>',
+            content: '<p>Заметили ошибку? Пишите в Обратную связь. Приятного использования!</p>',
             offsetX: X[device][i],
             offsetY: Y[device][j],
             isBeforeFuncExec: true,
